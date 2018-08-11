ff653127de8d608841a71c46ce6ca21e761d3af6
Miggi <miras.mustimov@tuta.io>
2018-08-11 14:37:42 +0600

    institution list for small devices

--- resources/views/institutions/index.blade.php
+++ resources/views/institutions/index.blade.php
@@ -118,8 +118,30 @@
         </div>
 
         <div>
-            <main>
-                {{--  --}}
+            <main class="container mx-auto">
+                <div class="p-3">
+                    {{-- Institutions list --}}
+                    @foreach ($institutions as $institution)
+                        <div class="flex mb-6">
+                            <div class="w-12 flex-no-shrink mr-2 p-1">
+                                @if ($institution->hasLogo())
+                                    <a href="{{ route('institutions.show', [ $institutionType, $institution ]) }}" class="block">
+                                        <img src="{{ asset($institution->logo()->getUrl()) }}" alt="{{ $institution->title }}" class="block w-full">
+                                    </a>
+                                @endif
+                            </div>
+
+                            <div class="flex-1 min-w-0 flex flex-col justify-center px-2 border-orange-light" style="border-left-width: 1.3px">
+                                <div class="mb-1">
+                                    <a href="{{ route('institutions.show', [ $institutionType, $institution ]) }}" class="text-blue-dark no-underline">{{ $institution->title }}</a>
+                                </div>
+                                <div class="text-sm text-grey-dark">
+                                    Город: {{ $institution->city->title }}
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
             </main>
         </div>
     </div>
