9adbd01af176a2214ca4fe22d0837de32c563b89
Miggi <miras.mustimov@tuta.io>
2018-08-26 14:10:23 +0600

    add ss select to the mobile views

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -87,26 +87,28 @@
                                    class="block w-full mb-4 text-grey px-6 placeholder-text-grey-dark"
                                    style="padding-top: .937rem; padding-bottom: .937rem;">
 
-                            <select class="block w-full mb-4 text-grey px-6 styled-select" name="specialty" style="padding-top: .937rem; padding-bottom: .937rem;">
-                                <option value="" class="py-1 px-6">
+                            <ss-select class="mb-4 text-grey" name="specialty">
+                                <input type="search" class="block w-full h-12 text-grey px-6" style="padding-top: .937rem; padding-bottom: .937rem;">
+                                <option value="" class="py-2 px-6">
                                     Специальности{{ $institutionType == 'colleges' ? ', квалификации' : '' }}
                                 </option>
                                 @foreach ($specialties as $specialty)
-                                    <option value="{{ $specialty->id }}" {{ request('specialty') == $specialty->id ? 'selected' : '' }} class="py-1 px-6">
+                                    <option value="{{ $specialty->id }}" {{ request('specialty') == $specialty->id ? 'selected' : '' }} class="py-2 px-6">
                                         {{ $specialty->title }} ({{ $specialty->code }})
                                     </option>
                                 @endforeach
-                            </select>
+                            </ss-select>
 
                             <div class="flex">
-                                <select class="flex-1 min-w-0 text-grey mr-2 px-6 styled-select" name="city" style="padding-top: .937rem; padding-bottom: .937rem;">
-                                    <option value="" class="px-6 py-1">Город</option>
+                                <ss-select class="flex-1 min-w-0 h-full text-grey mr-2" name="city">
+                                    <input type="search" class="block w-full h-12 text-grey px-6" style="padding-top: .937rem; padding-bottom: .937rem;">
+                                    <option value="" class="px-6 py-2">Город</option>
                                     @foreach ($cities as $city)
-                                        <option value="{{ $city->id }}" {{ request('city') == $city->id ? 'selected' : '' }} class="px-6 py-1">
+                                        <option value="{{ $city->id }}" {{ request('city') == $city->id ? 'selected' : '' }} class="px-6 py-2">
                                             {{ $city->title }}
                                         </option>
                                     @endforeach
-                                </select>
+                                </ss-select>
 
                                 <button class="block w-12 flex-no-shrink bg-blue-dark">
                                     <svg class="inline-block h-6 w-8 mt-1 fill-current text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.9 14.32a8 8 0 1 1 1.41-1.41l5.35 5.33-1.42 1.42-5.33-5.34zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z"/></svg>
