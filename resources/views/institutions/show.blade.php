3acb3330961deeb8b3a54e9178f53cc5c07628f7
Miggi <miras.mustimov@tuta.io>
2018-08-26 14:10:50 +0600

    mess up with container for lg devices

--- resources/views/institutions/show.blade.php
+++ resources/views/institutions/show.blade.php
@@ -10,8 +10,18 @@
 
         {{-- Контент страницы --}}
         <div class="flex-1 mt-px lg:bg-grey-light">
-            <main class="container mx-auto">
-                <div class="lg:bg-white lg:shadow-lg about-lg:mx-20">
+            <main>
+                <div class="hidden lg:block container mx-auto">
+                    <div class="lg:bg-white lg:shadow-lg about-lg:mx-20">
+                        <div>
+                            <div>
+                                <img src="/img/institutions/default-profile-photo.jpg" alt="{{ $institution->title }}" class="block w-full">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="lg:hidden">
                     @include ('institutions.partials.show.accordions')
                 </div>
             </main>
