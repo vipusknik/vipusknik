ee158a07a73e4ad034a97766d2c43f988497382c
Miggi <miras.mustimov@tuta.io>
2018-09-16 21:37:05 +0600

    +++

--- resources/views/institutions/partials/no-info.blade.php
+++ resources/views/institutions/partials/no-info.blade.php
@@ -1,3 +1,3 @@
-<div class="text-center italic lg:text-left bg-white">
+<div class="text-center italic lg:text-left bg-white mb-2 lg:mb-0">
     К сожалению, {{ $institution->title }} еще не предоставил информацию в данный раздел
 </div>
