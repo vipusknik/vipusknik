7c11731951b6192a3d8fc4b1450b974aa34d139f
Miggi <miras.mustimov@tuta.io>
2018-09-24 21:13:43 +0600

    +++

--- resources/views/institutions/partials/contacts.blade.php
+++ resources/views/institutions/partials/contacts.blade.php
@@ -3,7 +3,7 @@
     <div class="flex-none mr-2 lg:mr-3">
         <svg class="inline-block w-4 h-4 fill-current text-black lg:text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 20S3 10.87 3 7a7 7 0 1 1 14 0c0 3.87-7 13-7 13zm0-11a2 2 0 1 0 0-4 2 2 0 0 0 0 4z"/></svg>
     </div>
-    <div class="lg:text-white lg:text-xl">{{ $institution->address }}</div>
+    <div class="lg:text-white lg:text-xl">г. {{ $institution->city->title }}, {{ $institution->address }}</div>
 </div>
 @endif
 
