fa0eea3a0d53ad4230d1a437cb2381f7fe0f13f2
Miggi <miras.mustimov@tuta.io>
2018-10-21 20:25:05 +0600

    +++

--- resources/assets/js/components/Institution.vue
+++ resources/assets/js/components/Institution.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="flex mb-6">
         <div class="w-12 flex-no-shrink mr-2 p-1">
-            <a v-if="logo" :href="`/institutions/${institutionType}/${institution.slug}`" class="block">
+            <a v-if="logo" :href="`/institutions/${institutionType}/${institution.slug}`" class="block" target="_blank">
                 <img :src="logoLink" :alt="institution.title" class="block w-full">
             </a>
         </div>
 
         <div class="flex-1 min-w-0 flex flex-col justify-center px-2 border-orange-light" style="border-left-width: 1.3px">
             <div class="mb-1">
-                <a :href="`/institutions/${institutionType}/${institution.slug}`" class="text-blue-dark no-underline hover:underline">
+                <a :href="`/institutions/${institutionType}/${institution.slug}`" class="text-blue-dark no-underline hover:underline" target="_blank">
                     {{ institution.title }}
                 </a>
             </div>
--- resources/views/institutions/partials/institution-in-list.blade.php
+++ resources/views/institutions/partials/institution-in-list.blade.php
@@ -1,7 +1,7 @@
 <div class="flex mb-6">
     <div class="w-12 flex-no-shrink mr-2 p-1">
         @if ($institution->hasLogo())
-            <a href="{{ route('institutions.show', [ $institution->type, $institution ]) }}" class="block">
+            <a href="{{ route('institutions.show', [ $institution->type, $institution ]) }}" class="block" target="_blank">
                 <img src="{{ asset($institution->logo()->getUrl()) }}" alt="{{ $institution->title }}" class="block w-full">
             </a>
         @endif
@@ -9,7 +9,7 @@
 
     <div class="flex-1 min-w-0 flex flex-col justify-center px-2 border-orange-light" style="border-left-width: 1.3px">
         <div class="mb-1">
-            <a href="{{ route('institutions.show', [ $institution->type, $institution ]) }}" class="text-blue-dark no-underline hover:underline">
+            <a href="{{ route('institutions.show', [ $institution->type, $institution ]) }}" class="text-blue-dark no-underline hover:underline" target="_blank">
                 {{ $institution->title }}
             </a>
         </div>
