597af657a20496c97ba1a5675f61504537291461
miggi <miras.mustimov@tuta.io>
2019-05-20 10:21:37 +0600

    minus s in colleges

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -131,7 +131,7 @@
 
                     {{-- Баннер 2 --}}
                     <div class="mb-3 px-3">
-                        <a href="http://vipusknik.kz/institutions/colleges/ust-kamenogorskiy-mnogoprofilnyy-tehnologicheskiy-kolledzh-kgu" target="_blank">
+                        <a href="http://vipusknik.kz/institutions/college/ust-kamenogorskiy-mnogoprofilnyy-tehnologicheskiy-kolledzh-kgu" target="_blank">
                             <img src="/img/mnogopr.jpg" class="w-full h-auto">
                         </a>
                     </div>
